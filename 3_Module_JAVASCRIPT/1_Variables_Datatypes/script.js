1193599d79a184e689bfa6e2f45396a02be0d09f
Es <94657666+EsmeBergach@users.noreply.github.com>
2022-03-14 23:44:02 +0100

    Assignment_2

--- 3_Module_JAVASCRIPT/1_Variables_Datatypes/script.js
+++ 3_Module_JAVASCRIPT/1_Variables_Datatypes/script.js
@@ -1,4 +1,100 @@
+//Part 1:Objects
+// let person = {
+//     name:'Esme',
+//     age:32
+// };
+// person['age'] = 25;
 
+// console.log(person.age);
+
+//Part 2: Arrays
+
+// let evaluations = ['red', 'blue', 'green'];
+// evaluations[3] = 'black';
+// evaluations.push('pink');
+// evaluations.push('5');
+// evaluations.push('Hi, I am an object');
+
+
+// console.log(evaluations[6]);
+
+
+//Part 3: Real life object
+
+const catBreeds = [{
+    name: "Abyssinian",
+    description: "The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.",
+    dog_friendly: 4,
+    energy_level: 5,
+    life_span: "14 - 15",
+    origin: "Egypt",
+    temperament: ["Active", "Energetic", "Independent", "Intelligent", "Gentle"],
+    wikipedia_url: "https://en.wikipedia.org/wiki/Abyssinian_(cat)",
+    food: {favourite_food: "fish",
+    medium_liked_food: "dried fruits", 
+    disliked_food: "walnuts"
+    }},
+    {name: "Aegean",
+    description: "Native to the Greek islands known as the Cyclades in the Aegean Sea, these are natural cats, meaning they developed without humans getting involved in their breeding. As a breed, Aegean Cats are rare, although they are numerous on their home islands. They are generally friendly toward people and can be excellent cats for families with children.",
+    dog_friendly: 4,
+    energy_level: 53,
+    life_span: "9- 12",
+    origin: "Greece",
+    temperament: [ "Affectionate", "Social", "Intelligent", "Playful", "Active"],
+    wikipedia_url: "https://en.wikipedia.org/wiki/Aegean_cat",
+    food: {favourite_food: "tuna",
+    medium_liked_food: "canned food", 
+    disliked_food: "all fruits"
+    }},
+    {name: "American Bobtail",
+    description: "American Bobtails are loving and incredibly intelligent cats possessing a distinctive wild appearance. They are extremely interactive cats that bond with their human family with great devotion.",
+    dog_friendly: 5,
+    energy_level: 3,
+    life_span: "11 - 15",
+    origin: "United States",
+    temperament: [ "Intelligent", "Interactive", "Lively", "Playful", "Sensitive"],
+    wikipedia_url: "https://en.wikipedia.org/wiki/American_Bobtail",
+    food: {favourite_food: "meaty things",
+    medium_liked_food: "tuna", 
+    disliked_food: "canned food"
+    }}
+    ]
+    console.log("Favourite food of catbreed 3:", catBreeds[2].food.favourite_food);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Reference types 
+// let person = {
+//     name:"Esme", 
+//     age:32
+// };
+
+// // Dot Notation 
+// person.name ="John";
+// //Bracket Notation 
+// person ['name'] = 'Mary';
+ 
+// console.log (person);
+
+
+
+// Part Age Variable
 // var myAge = 26;
 
 // if (myAge < 18 || myAge > 30 || myAge === 25){
@@ -9,79 +105,57 @@
 //     document.write("Ya cool dude, come in");
 // }
 
-// Part 1
+// // Part 1
 
-const age = 26;
-const firstName = "Bramm";
-const isFemale = true;
-const driverStatus ="designated";
-const totalAmount = 15;
+// const age = 26;
+// const firstName = "Bramm";
+// const isFemale = true;
+// const driverStatus ="designated";
+// const totalAmount =115;
 
-if (age >= 18) {
-    document.write("You're 18 or older.Welcome in the pub!");
-}
-else if (age <= 18) {
-    document.write("You're under 18. You cannot enter the pub");
-}
-
-if (age >= 18 && age <= 25) {
-    document.write("Welcome, You get a 50% discount!");
-}
-
-if (firstName === "Bram" || firstName === "Sarah") {
-    document.write("You're Sarah or Bram, You get free beers forever!");
-}
-else {
-    document.write("Sorry your parents should have called you differently.")
-}
-
-if (isFemale){
-    document.write("You are a female.Buy your ticket here");
-}
-else {
-    document.write("This ticket is for females only");
-}
-
-if (driverStatus =="designated") {
-    document.write("You are allowed to drive.");
-}
-else {
-    document.write("Not allowed to drive");
-}
-
-if (totalAmount >= 100) {
-    document.write("Whoop, champange on the house!")
-}
-else if (totalAmount > 50) {
-    document.write("You've spent over 50. Nacho's on the house!")
-}
-else if (totalAmount >= 25) {
-    document.write("You've spent over 25. You get a free portion of bitterballen!")
-}
-else if (totalAmount < 25){
-    console.log("No discounts. You havent spent over 25.")
-}
-
-// console.log ("Hola world and students");
-
-// var myAge = 19;
-
-// if (myAge > 30){
-//     document.write("you're old");
-
+// if (age >= 18) {
+//     document.write("You're 18 or older.Welcome in the pub!");
+// }
+// else if (age <= 18) {
+//     document.write("You're under 18. You cannot enter the pub");
 // }
 
-// else if (myAge > 20) {
-//     document.write("you're over 20");
-
+// if (age >= 18 && age <= 25) {
+//     document.write("Welcome, You get a 50% discount!");
 // }
 
-// else if (myAge > 10){
-//     document.write ("you are so young");
+// if (firstName === "Bram" || firstName === "Sarah") {
+//     document.write("You're Sarah or Bram, You get free beers forever!");
 // }
-
 // else {
-//     document.write("get back to school");
+//     document.write("Sorry your parents should have called you differently.")
+// }
+
+// if (isFemale){
+//     document.write("You are a female.Buy your ticket here");
+// }
+// else {
+//     document.write("This ticket is for females only");
+// }
+
+// if (driverStatus =="designated") {
+//     document.write("You are allowed to drive.");
+// }
+// else {
+//     document.write("Not allowed to drive");
+// }
+
+// if (totalAmount >= 100) {
+//     document.write("Whoop, champange on the house!")
+// }
+// else if (totalAmount > 50) {
+//     document.write("You've spent over 50. Nacho's on the house!")
+// }
+// else if (totalAmount >= 25) {
+//     document.write("You've spent over 25. You get a free portion of bitterballen!")
+// }
+// else if (totalAmount < 24){
+//     document.write("No discounts. You havent spent over 25.")
 // }
 
 
---
+++ 3_Module_JAVASCRIPT/2_Function_Basics/script.js
@@ -0,0 +1,39 @@
+
+//Function:Arguements
+// const paintWall = function (paintColor, wallSide){
+// console.log(wallSide + paintColor)
+// };
+
+
+// const paintRed = 'red';
+// const paintOrange = 'orange';
+// const paintGray = 'gray';
+// const wallNorth ='The north wall has been painted ';
+// const wallSouth ='The South-east wall has been painted ';
+
+// paintWall(paintOrange, wallNorth);
+
+
+
+
+
+
+// //Function: Basics
+// const makePizza = function (){
+//     console.log(makeDough);
+//     console.log(dressPizza);
+//     console.log(bakePizza);
+// }
+
+// const makeSushi = function(){
+//     console.log(buyFish);
+//     console.log(makeRice);
+//     console.log(createRolls);
+// }
+
+
+// console.log(makePizza);
+// console.log(makePizza);
+// console.log(makeSushi);
+// console.log(makeSushi);
+// console.log(makeSushi);
