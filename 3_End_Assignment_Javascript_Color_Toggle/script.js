7e4bfaa70850d5204373d89bb8e6d4f4bae03d67
Es <94657666+EsmeBergach@users.noreply.github.com>
2022-03-28 13:33:21 +0200

    assignment_toggle

--- 3_End_Assignment_Javascript_Color_Toggle/script.js
+++ 3_End_Assignment_Javascript_Color_Toggle/script.js
@@ -1,1 +1,30 @@
-// const hamburger = document.
+let toggleNavStatus = false;
+
+let toggleNav = function () {
+  let getSidebar = document.querySelector(".nav-main");
+  let getSidebarUl = document.querySelector(".nav-main ul");
+  let getSidebarLinks = document.querySelectorAll(".nav-main a");
+
+  if (toggleNavStatus === false) {
+    getSidebarUl.style.visibility = "visible";
+    getSidebar.style.width = "150px";
+
+    let arrayLength = getSidebarLinks.length;
+    for (let i = 0; i < arrayLength; i++) {
+      getSidebarLinks[i].style.opacity = "1";
+    }
+
+    toggleNavStatus = true;
+  } else if (toggleNavStatus === true) {
+    getSidebar.style.width = "40px";
+
+    let arrayLength = getSidebarLinks.length;
+    for (let i = 0; i < arrayLength; i++) {
+      getSidebarLinks[i].style.opacity = "0";
+    }
+
+    getSidebarUl.style.visibility = "hidden";
+
+    toggleNavStatus = false;
+  }
+};
